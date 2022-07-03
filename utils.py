107dac7d87ea2e04495275adec70093ba72d3ea9
BityutskiyNA <BityutskiyNA@gmail.com>
2022-07-03 17:27:03 +0300

    first

---
+++ utils.py
@@ -0,0 +1,29 @@
+
+
+
+def load_candidates_from_json(path):
+    """
+     – возвращает список всех кандидатов
+    """
+    return ""
+
+
+def get_candidate(candidate_id):
+    """
+    – возвращает одного кандидата по его id
+    """
+    return ""
+
+
+def get_candidates_by_name(candidate_name):
+    """
+    ` – возвращает кандидатов по имени
+    """
+    return ""
+
+
+def get_candidates_by_skill(skill_name):
+    """
+    – возвращает кандидатов по навыку
+    """
+    return ""
